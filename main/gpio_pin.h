d8f70dc09faa1b92744891cf3a604b14f25f82db
Sahil Khan S J <sahil.k@noveloffice.in>
2026-02-14 11:20:20 +0530

    Changed the buzzer pins

--- main/gpio_pin.h
+++ main/gpio_pin.h
@@ -6,11 +6,11 @@
 // -------- PIN DEFINITIONS --------
 #define GREEN_LED_1   GPIO_NUM_25
 #define RED_LED_1     GPIO_NUM_32
-#define BUZZER_1      GPIO_NUM_33
+#define BUZZER_1      GPIO_NUM_22
 
 #define GREEN_LED_2   GPIO_NUM_13
 #define RED_LED_2     GPIO_NUM_14
-#define BUZZER_2      GPIO_NUM_22
+#define BUZZER_2      GPIO_NUM_33
 
 #define RELAY_1       GPIO_NUM_23
 
